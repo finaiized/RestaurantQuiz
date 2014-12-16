d903e4626bac76b38d3299c0e0aff23753f01cbe
Dominic Kuang <finaiized@gmail.com>
2014-12-16 10:40:15 -0800

    Initial Commit

---
+++ Restaurant Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant Quiz
+//
+//  Created by Dominic Kuang on 12/16/14.
+//  Copyright (c) 2014 Dominic Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
