1b566fce5a64fbbef6d59f97e58f8e0bd2998cce
Dominic Kuang <finaiized@gmail.com>
2014-12-16 13:26:15 -0800

    Basic testing of MapKit

--- Restaurant Quiz/ViewController.swift
+++ Restaurant Quiz/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
+        let directionRequest = MKDirectionsRequest()
+        let ladner = CLLocationCoordinate2D(latitude: 49.081241, longitude: -123.083793)
+        let ladnerPlaceMark = MKPlacemark(coordinate: ladner, addressDictionary: nil)
+        let ladnerMapItem = MKMapItem(placemark: ladnerPlaceMark)
+        let van = CLLocationCoordinate2D(latitude: 49.266638, longitude: -123.249275)
+        let vanPlaceMark = MKPlacemark(coordinate: van, addressDictionary: nil)
+        let vanMapItem = MKMapItem(placemark: vanPlaceMark)
+        
+        directionRequest.setSource(ladnerMapItem)
+        directionRequest.setDestination(vanMapItem)
+        directionRequest.transportType = MKDirectionsTransportType.Automobile
+        
+        let point = MKPointAnnotation()
+        point.coordinate = ladner
+        mapView.addAnnotation(point)
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculateDirectionsWithCompletionHandler({
+            [unowned self](response: MKDirectionsResponse!, error: NSError!) in
+            let route = response.routes[0] as MKRoute
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.AboveRoads)
+        })
+        
+        let region = MKCoordinateRegionMakeWithDistance(ladner, 1000, 1000)
+        mapView.region = region
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            let polyRenderer = MKPolylineRenderer(overlay: overlay)
+            polyRenderer.lineWidth = 3
+            polyRenderer.strokeColor = UIColor.redColor().colorWithAlphaComponent(0.6)
+            return polyRenderer
+        }
+        return nil
+    }
 }
 
