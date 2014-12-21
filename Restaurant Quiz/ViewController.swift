eea985d70b1204b717ae70072eab2bedd6b9102c
Dominic Kuang <finaiized@gmail.com>
2014-12-20 16:13:30 -0800

    Code reorganization

--- Restaurant Quiz/ViewController.swift
+++ Restaurant Quiz/ViewController.swift
@@ -11,8 +11,11 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate, NSURLConnectionDelegate {
     
+    // MARK: - Outlets
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var distanceLabel: UILabel!
+    
+    // MARK: - Properties
     
     // The distance, from the destination, considered to be close enough
     let winningDistance = 100.0
@@ -44,6 +47,7 @@ class ViewController: UIViewController, MKMapViewDelegate, NSURLConnectionDelega
         }
     }
     
+    // MARK: - Lifecycle Methods
     required init(coder aDecoder: NSCoder) {
         overlays = []
         restaurants = []
@@ -65,125 +69,46 @@ class ViewController: UIViewController, MKMapViewDelegate, NSURLConnectionDelega
         self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
-    /** Returns the directions to point, starting from the most recent pin, if any */
-    func directionsToNewPoint(point: MKMapItem) -> MKDirections {
-        // First pin dropped
-        if current == nil {
-            let temp = MKPointAnnotation()
-            temp.coordinate = point.placemark.coordinate
-            current = temp
-        }
-        
-        let directionRequest = MKDirectionsRequest()
-        directionRequest.setSource(mapItemFrom(annotation: current!))
-        directionRequest.setDestination(point)
-        
-        let prev = MKPointAnnotation()
-        prev.coordinate = point.placemark.coordinate
-        
-        return MKDirections(request: directionRequest)
-    }
-    
-    /** Convenience method to convert an annotation to a MKMapItem */
-    func mapItemFrom(#annotation: MKAnnotation) -> MKMapItem {
-        return MKMapItem(placemark: MKPlacemark(coordinate: annotation.coordinate, addressDictionary: nil))
-    }
-    
-    /** Draw a route. Use as a completion handler. */
-    func drawRoute(response: MKDirectionsResponse!, error: NSError!) {
-        let route = response.routes[0] as MKRoute
-        distance += route.distance
-        overlays.append(route.polyline)
-        self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.AboveRoads)
-    }
-    
-    
-    // MARK: - MKMapViewDelegate
-    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
-        if overlay is MKPolyline {
-            let polyRenderer = MKPolylineRenderer(overlay: overlay)
-            polyRenderer.lineWidth = 3
-            polyRenderer.strokeColor = UIColor.redColor().colorWithAlphaComponent(0.6)
-            return polyRenderer
-        }
-        return nil
-    }
-    
-    // MARK: -
-    func handleMapTouch(gr: UITapGestureRecognizer) {
-        if gr.state == UIGestureRecognizerState.Began && playing {
-            let mapCoord = mapView.convertPoint(gr.locationInView(mapView), toCoordinateFromView: mapView)
-            mapView.removeAnnotation(previous)
-            previous = current
-            
-            panCameraTo(mapCoord, heading: CLLocationDegrees(arc4random_uniform(360)))
-            
-            let dir = directionsToNewPoint(MKMapItem(placemark: MKPlacemark(coordinate: mapCoord, addressDictionary: nil)))
-            dir.calculateDirectionsWithCompletionHandler(drawRoute)
-            
-            if distanceBetweenPoints(destination!.coordinate, p2: mapCoord) >= winningDistance {
-                let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: mapCoord, addressDictionary: nil))
-                current = addAnnotation(mapItem)
-            } else {
-                endGame()
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let id = segue.identifier {
+            if id == "Category" {
+                let cpvc = segue.destinationViewController as DDCategoryPickerController
+                
+                var categories = NSMutableSet()
+                for r in restaurants {
+                    for c in r.categories {
+                        categories.addObject(c)
+                    }
+                }
+                
+                cpvc.categories = categories.allObjects as [String]
             }
         }
-        
     }
     
-    /** Adds an annotation to the map, including the distance to destination */
-    func addAnnotation(coord: MKMapItem) -> MKAnnotation {
-        let point = MKPointAnnotation()
-        point.coordinate = coord.placemark.coordinate
-        mapView.addAnnotation(point)
-        
-        let dist = distanceBetweenPoints(destination!.coordinate, p2: point.coordinate)
-        
-        point.title = "\(distanceFormatter.stringFromDistance(dist))"
-        return point
+    // MARK: - Game Methods
+    
+    /** Start a new round of the game */
+    func startNewRound(destinationRestaurant: Restaurant) {
+        playing = true
+        resetMapState()
+        destination = destinationRestaurant
+        panCameraTo(destinationRestaurant.city)
+    }
+
+    func endGame() {
+        let alert = UIAlertController(title: "Congratulations!", message: "You have won after travelling \(distanceFormatter.stringFromDistance(distance)).", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
+            (alertAction: UIAlertAction!) in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        playing = false
+        self.presentViewController(alert, animated: true, completion: nil)
+        mapView.removeAnnotations(mapView.annotations)
+        addAnnotation(destination!)
     }
     
-    /** Adds an annotation at the restaurant, including its name */
-    func addAnnotation(restaurant: Restaurant) -> MKAnnotation {
-        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: restaurant.coordinate, addressDictionary: nil))
-        let point = addAnnotation(mapItem) as MKPointAnnotation
-        point.title = restaurant.name
-        return point
-    }
-    
-    /** Get the straight line distance between two points, in metres*/
-    func distanceBetweenPoints(p1: CLLocationCoordinate2D, p2: CLLocationCoordinate2D) -> CLLocationDistance {
-        let dest = CLLocation(latitude: p1.latitude, longitude: p1.longitude)
-        let currentPoint = CLLocation(latitude: p2.latitude, longitude: p2.longitude)
-        let dist = currentPoint.distanceFromLocation(dest)
-        
-        return dist
-    }
-    
-    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("ColourAnnotationView") as? ColourAnnotationView
-        
-        if annotationView == nil {
-            annotationView = ColourAnnotationView(annotation: annotation, reuseIdentifier: "ColourAnnotationView")
-            annotationView!.canShowCallout = true
-            annotationView!.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
-        } else {
-            annotationView!.annotation = annotation
-        }
-        
-        let dist = distanceBetweenPoints(annotation.coordinate, p2: destination!.coordinate)
-        annotationView!.hue = colourGradientFromDistanceRemaining(dist)
-        annotationView!.setNeedsDisplay()
-        return annotationView
-    }
-    
-    /** Returns a float representing a colour between red and green, scaling linearly from 0 to 3000 respectively */
-    func colourGradientFromDistanceRemaining(distance: CLLocationDistance) -> Float {
-        if distance >= 3000 {
-            return 0
-        }
-        return Float(M_PI - (M_PI * distance / 3000))
-    }
+    // MARK: - Restaurant Methods
     
     /** Called when city is selected in DDCityViewController */
     func getRestaurantsInCity(city: DDCity) {
@@ -215,13 +140,111 @@ class ViewController: UIViewController, MKMapViewDelegate, NSURLConnectionDelega
         self.startNewRound(restaurant)
     }
     
-    /** Start a new round of the game */
-    func startNewRound(destinationRestaurant: Restaurant) {
-        playing = true
-        resetMapState()
-        destination = destinationRestaurant
-        panCameraTo(destinationRestaurant.city)
+    // MARK: - Routing Methods
+    /** Returns the directions to point, starting from the most recent pin, if any */
+    func directionsToNewPoint(point: MKMapItem) -> MKDirections {
+        // First pin dropped
+        if current == nil {
+            let temp = MKPointAnnotation()
+            temp.coordinate = point.placemark.coordinate
+            current = temp
+        }
+        
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.setSource(mapItemFrom(annotation: current!))
+        directionRequest.setDestination(point)
+        
+        let prev = MKPointAnnotation()
+        prev.coordinate = point.placemark.coordinate
+        
+        return MKDirections(request: directionRequest)
     }
+    
+    /** Draw a route. Use as a completion handler. */
+    func drawRoute(response: MKDirectionsResponse!, error: NSError!) {
+        let route = response.routes[0] as MKRoute
+        distance += route.distance
+        overlays.append(route.polyline)
+        self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.AboveRoads)
+    }
+    
+    
+    /** Get the straight line distance between two points, in metres*/
+    func distanceBetweenPoints(p1: CLLocationCoordinate2D, p2: CLLocationCoordinate2D) -> CLLocationDistance {
+        let dest = CLLocation(latitude: p1.latitude, longitude: p1.longitude)
+        let currentPoint = CLLocation(latitude: p2.latitude, longitude: p2.longitude)
+        let dist = currentPoint.distanceFromLocation(dest)
+        
+        return dist
+    }
+    
+    /** Returns a float representing a colour between red and green, scaling linearly from 0 to 3000 respectively */
+    func colourGradientFromDistanceRemaining(distance: CLLocationDistance) -> Float {
+        if distance >= 3000 {
+            return 0
+        }
+        return Float(M_PI - (M_PI * distance / 3000))
+    }
+    
+    /** Convenience method to convert an annotation to a MKMapItem */
+    func mapItemFrom(#annotation: MKAnnotation) -> MKMapItem {
+        return MKMapItem(placemark: MKPlacemark(coordinate: annotation.coordinate, addressDictionary: nil))
+    }
+    
+    
+    // MARK: - MKMapViewDelegate
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            let polyRenderer = MKPolylineRenderer(overlay: overlay)
+            polyRenderer.lineWidth = 3
+            polyRenderer.strokeColor = UIColor.redColor().colorWithAlphaComponent(0.6)
+            return polyRenderer
+        }
+        return nil
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("ColourAnnotationView") as? ColourAnnotationView
+        
+        if annotationView == nil {
+            annotationView = ColourAnnotationView(annotation: annotation, reuseIdentifier: "ColourAnnotationView")
+            annotationView!.canShowCallout = true
+            annotationView!.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
+        } else {
+            annotationView!.annotation = annotation
+        }
+        
+        let dist = distanceBetweenPoints(annotation.coordinate, p2: destination!.coordinate)
+        annotationView!.hue = colourGradientFromDistanceRemaining(dist)
+        annotationView!.setNeedsDisplay()
+        return annotationView
+    }
+    
+    // MARK: - Gesture Recognizer
+    func handleMapTouch(gr: UITapGestureRecognizer) {
+        if gr.state == UIGestureRecognizerState.Began && playing {
+            let mapCoord = mapView.convertPoint(gr.locationInView(mapView), toCoordinateFromView: mapView)
+            mapView.removeAnnotation(previous)
+            previous = current
+            
+            panCameraTo(mapCoord, heading: CLLocationDegrees(arc4random_uniform(360)))
+            
+            let dir = directionsToNewPoint(MKMapItem(placemark: MKPlacemark(coordinate: mapCoord, addressDictionary: nil)))
+            dir.calculateDirectionsWithCompletionHandler(drawRoute)
+            
+            if distanceBetweenPoints(destination!.coordinate, p2: mapCoord) >= winningDistance {
+                let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: mapCoord, addressDictionary: nil))
+                current = addAnnotation(mapItem)
+            } else {
+                endGame()
+            }
+        }
+        
+    }
+    
+
+
+    // MARK: - MapView Helpers
     
     /** Reset the map and markers to an unplayed state */
     func resetMapState() {
@@ -232,18 +255,6 @@ class ViewController: UIViewController, MKMapViewDelegate, NSURLConnectionDelega
         overlays = []
         distance = 0
         destination = nil
-    }
-    
-    func endGame() {
-        let alert = UIAlertController(title: "Congratulations!", message: "You have won after travelling \(distanceFormatter.stringFromDistance(distance)).", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
-            (alertAction: UIAlertAction!) in
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }))
-        playing = false
-        self.presentViewController(alert, animated: true, completion: nil)
-        mapView.removeAnnotations(mapView.annotations)
-        addAnnotation(destination!)
     }
     
     /** Animates the camera to the given city */
@@ -260,21 +271,24 @@ class ViewController: UIViewController, MKMapViewDelegate, NSURLConnectionDelega
         mapView.setCamera(camera, animated: true)
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let id = segue.identifier {
-            if id == "Category" {
-                let cpvc = segue.destinationViewController as DDCategoryPickerController
-                
-                var categories = NSMutableSet()
-                for r in restaurants {
-                    for c in r.categories {
-                        categories.addObject(c)
-                    }
-                }
-                
-                cpvc.categories = categories.allObjects as [String]
-            }
-        }
+    
+    /** Adds an annotation to the map, including the distance to destination */
+    func addAnnotation(coord: MKMapItem) -> MKAnnotation {
+        let point = MKPointAnnotation()
+        point.coordinate = coord.placemark.coordinate
+        mapView.addAnnotation(point)
+        
+        let dist = distanceBetweenPoints(destination!.coordinate, p2: point.coordinate)
+        
+        point.title = "\(distanceFormatter.stringFromDistance(dist))"
+        return point
+    }
+    
+    /** Adds an annotation at the restaurant, including its name */
+    func addAnnotation(restaurant: Restaurant) -> MKAnnotation {
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: restaurant.coordinate, addressDictionary: nil))
+        let point = addAnnotation(mapItem) as MKPointAnnotation
+        point.title = restaurant.name
+        return point
     }
 }
-
